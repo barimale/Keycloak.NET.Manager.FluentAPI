c497eb4210fcc524a84212bc74849e8119fa577f
barimale <mateusz.wolnica@gmail.com>
2019-12-12 14:25:27 +0100

    .

--- UT.Keycloak.NET.Manager.FluentAPI/InputData.cs
+++ UT.Keycloak.NET.Manager.FluentAPI/InputData.cs
@@ -6,7 +6,6 @@
         public static string Password = "bar";
         public static string Endpoint = "http://localhost:8085/";
         public static string Realm = "DEV";
-
         public static string ClientId = "Confidential";
         public static string ClientSecret = "8a03e418-bf2d-42a4-b9d1-7d8f1ed633d5";
     }
