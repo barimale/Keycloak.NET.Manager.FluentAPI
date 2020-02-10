9b9d4c21424ff8ed3b6c2fe3ff2600733440346a
barimale <mateusz.wolnica@gmail.com>
2020-02-10 15:30:03 +0100

    .

--- Keycloak.NET.Manager.FluentAPI/Keycloak.NET.Manager.FluentAPI/Configurator/Roles/IRoles.cs
+++ Keycloak.NET.Manager.FluentAPI/Keycloak.NET.Manager.FluentAPI/Configurator/Roles/IRoles.cs
@@ -6,6 +6,7 @@ namespace Keycloak.NET.FluentAPI.Configure
     public interface IRoles
     {
         Task<bool> DeleteRoleAsync(string roleName);
+        Task<bool> DeleteRoleAsync(string roleName, string clientId);
         Task<bool> AddRoleAsync(string roleName);
         Task<bool> AddRoleAsync(string roleName, string clientId);
         Task<List<Net.Models.Roles.Role>> AllAsync();
--- Keycloak.NET.Manager.FluentAPI/Keycloak.NET.Manager.FluentAPI/Configurator/Roles/Role.cs
+++ Keycloak.NET.Manager.FluentAPI/Keycloak.NET.Manager.FluentAPI/Configurator/Roles/Role.cs
@@ -33,6 +33,21 @@ namespace Keycloak.NET.FluentAPI.Configure
             }
         }
 
+        public async Task<bool> DeleteRoleAsync(string roleName, string clientId)
+        {
+            try
+            {
+                return await _client.DeleteRoleByNameAsync(
+                    _context.ConnectionSettings.Realm,
+                    clientId,
+                    roleName);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
         public async Task<bool> AddRoleAsync(string roleName)
         {
             try
