3bca619cafc4123ef864b5fdeb4c46c357782dc3
barimale <mateusz.wolnica@gmail.com>
2020-03-05 21:04:39 +0100

    .

---
+++ Keycloak.NET.Manager.FluentAPI/Keycloak.NET.Manager.FluentAPI/Model/AttributedRole.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Keycloak.NET.Client.FluentAPI.Model
+{
+    [JsonObject]
+    public class AtributedRole
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("composite")]
+        public bool? Composite { get; set; }
+        [JsonProperty("clientRole")]
+        public bool? ClientRole { get; set; }
+        [JsonProperty("containerId")]
+        public string ContainerId { get; set; }
+        [JsonProperty("attributes")]
+        public Dictionary<string,string> Attributes { get; set; }
+    }
+}
